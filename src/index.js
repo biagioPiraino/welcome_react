26e19d5fe30a47224554a0512c60ad159852efb9
Biagio Piraino <biagiopirainowork@gmail.com>
2022-07-29 09:06:29 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Welcome from './static_pages/welcome/Welcome'
+import './index.css';
+
+ReactDOM.render(
+  <Welcome />,
+  document.getElementById('root')
+);
---
+++ src/static_pages/welcome/Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Welcome.css';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="Welcome">
+        <div className="Welcome-header">
+          <h1>Hello, React!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Welcome;

b22d0b0ac36ddbc9a1072e80fb69f34bf6e47c3b
Biagio Piraino <biagiopirainowork@gmail.com>
2022-07-31 10:17:05 +0100

    implemented first challenge with unordered list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Welcome from './static_pages/welcome/Welcome'
+import Unordered_list_00 from './static_pages/challenges/00_unordered_list/Unordered_list_00';
 import './index.css';
 
 ReactDOM.render(
-  <Welcome />,
+  [<Welcome />, <Unordered_list_00 />],
   document.getElementById('root')
 );
+
---
+++ src/static_pages/challenges/00_unordered_list/Unordered_list_00.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Unordered_list_00.css';
+
+class Unordered_list_00 extends Component {
+  render() {
+    return (
+      <div className="unordered-list-00">
+        <div className="unordered-list-00-list">
+          <ul>Unordered list as a first challenge
+            <li></li>
+            <li>Study React</li>
+            <li>Develop an application</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Unordered_list_00;

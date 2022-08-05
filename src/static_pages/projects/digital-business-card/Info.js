9adac80c5497553632dc84d91476d24d8d6709ea
Biagio <biagiopirainowork@gmail.com>
2022-08-05 08:34:28 +0100

    info section implemented

--- src/static_pages/projects/digital-business-card/Info.js
+++ src/static_pages/projects/digital-business-card/Info.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import logo from '../../../logo.svg'
+import profilepic from '../../../profilepic.jpeg'
 import './Info.css'
 
 class Info extends Component {
   render() {
     return (
       <div className='info'>
-         <img src={logo} className="info--user-img" alt="logo"/>
-         <h1 className="info--name">Biagio Piraino</h1>
-         <h3 className="info--role">.Net Developer</h3>
-         <small className="info--website">biagiopiraino.net</small>
+          <img src={profilepic} className="info--user-img" alt="logo"/>
+          <h1 className="info--name">Biagio Piraino</h1>
+          <h3 className="info--role">.Net Developer</h3>
+          <small className="info--website">biagiopiraino.net</small>
+          <div className="info--button-section">
+            <button className="info--button" id='email'>Email</button>
+            <button className="info--button" id='linkedin'>LinkedIn</button>
+          </div>
       </div>
     );
   }

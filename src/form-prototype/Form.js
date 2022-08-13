30360e98ad170174d8c1dff85c57c33fb648f215
biagio <biagiopirainowork@gmail.com>
2022-08-13 21:40:43 +0100

    generic form, basic structure defined

---
+++ src/form-prototype/Form.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const inputToDispay = this.props.inputs.map(
+      x =>
+      <React.Fragment>
+        <input
+          key={x.name}
+          className='form--input'
+          type={x.type}
+          name={x.name}
+          placeholder={x.placeholder}
+          onChange={this.props.onInputChange}
+        />
+      </React.Fragment>
+    );
+
+    return (
+      <form className="form--main">
+        {inputToDispay}
+      </form>
+    )
+  };
+}
+
+export default Form;
